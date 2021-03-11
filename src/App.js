3fc46227e6fdb1c4f68bffe8f1f6af30ee37052e
apam <antonio.mendes@xpand-it.com>
2021-03-11 18:09:07 +0000

    added checkout page, checkout item component and updated cart to use selectores from reselect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
-import "./App.css";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
 import SignInSignOutPage from "./pages/signIn-signUp/signIn-signUp.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
+
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selectors";
+
+import "./App.css";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -41,6 +46,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             path="/signIn"
             render={() =>
@@ -57,8 +63,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
-import { createStore, applyMiddleware } from "redux";
-import { logger } from "redux-logger";
+import { createStore } from "redux";
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+// import { applyMiddleware } from "redux";
+// import { logger } from "redux-logger";
+// const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+  rootReducer,
+  //   applyMiddleware(...middlewares),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
