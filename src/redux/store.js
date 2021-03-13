4ee8e11b6f3b87f605166e713307637f446062b8
apam <antonio.mendes@xpand-it.com>
2021-03-13 21:23:51 +0000

    moved our directory, shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,15 @@
 import { createStore } from "redux";
+import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
 // import { applyMiddleware } from "redux";
 // import { logger } from "redux-logger";
 // const middlewares = [logger];
 
-const store = createStore(
+export const store = createStore(
   rootReducer,
   //   applyMiddleware(...middlewares),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-export default store;
+export const persistor = persistStore(store);
